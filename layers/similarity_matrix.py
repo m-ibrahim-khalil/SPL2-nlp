7e50ae8d07374f164206d54dc1c278ff3db05bad
Ibrahim Khalil <mik858692@gmail.com>
2021-08-27 21:40:57 +0600

    Update similarity_matrix.py

--- layers/similarity_matrix.py
+++ layers/similarity_matrix.py
@@ -1,12 +1,21 @@
+"""
+    Similarity matrix inputs the contextual embedding of (context and query)
+    Find the similarity between the context and query
+    output a (context_length, query_length) matrix
+"""
+
 from keras.layers import Layer
 import tensorflow as tf
-from keras import backend as K
-from keras.layers.advanced_activations import Softmax
+from keras import backend as k
 
 
 class SimilarityMatrix(Layer):
     def __init__(self, **kwargs):
         super(SimilarityMatrix, self).__init__(**kwargs)
+        self.context_shape = None
+        self.question_shape = None
+        self.kernel = None
+        self.similarity_matrix = None
 
     def build(self, input_shape):
         self.context_shape = input_shape[0]
@@ -23,17 +32,17 @@ class SimilarityMatrix(Layer):
         element_wise_multiply = context_vectors * query_vectors
         concatenated_tensor = tf.concat(
             [context_vectors, query_vectors, element_wise_multiply], axis=-1)
-        similarity = K.squeeze(K.dot(concatenated_tensor, self.kernel), axis=-1)
+        similarity = k.squeeze(k.dot(concatenated_tensor, self.kernel), axis=-1)
 
         return similarity
 
     def build_similarity_matrix(self, context, question):
-        num_context_words = K.shape(context)[1]
-        num_query_words = K.shape(question)[1]
-        context_dim_repeat = K.concatenate([[1, 1], [num_query_words], [1]], 0)
-        query_dim_repeat = K.concatenate([[1], [num_context_words], [1, 1]], 0)
-        context_vectors = K.tile(K.expand_dims(context, axis=2), context_dim_repeat)
-        query_vectors = K.tile(K.expand_dims(question, axis=1), query_dim_repeat)
+        num_context_words = k.shape(context)[1]
+        num_query_words = k.shape(question)[1]
+        context_dim_repeat = k.concatenate([[1, 1], [num_query_words], [1]], 0)
+        query_dim_repeat = k.concatenate([[1], [num_context_words], [1, 1]], 0)
+        context_vectors = k.tile(k.expand_dims(context, axis=2), context_dim_repeat)
+        query_vectors = k.tile(k.expand_dims(question, axis=1), query_dim_repeat)
         similarity_matrix = self.calculate_similarity(context_vectors, query_vectors)
         return similarity_matrix
 
@@ -43,4 +52,4 @@ class SimilarityMatrix(Layer):
         return self.similarity_matrix
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0][0], input_shape[0][1], input_shape[1][1])
+        return input_shape[0][0], input_shape[0][1], input_shape[1][1]
