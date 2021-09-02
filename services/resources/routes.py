4406a52345f926dfa816b9312b2e3a421ab9cc7e
Ibrahim Khalil <mik858692@gmail.com>
2021-09-02 22:41:16 +0600

    Update routes.py

--- services/resources/routes.py
+++ services/resources/routes.py
@@ -1,4 +1,3 @@
-from .movie import MoviesApi, MovieApi
 from .auth import SignupApi, LoginApi
 from .context_question import ContextQuestionsApi, ContextQuestionApi, AnswersApi
 
@@ -6,8 +5,6 @@ from .context_question import ContextQuestionsApi, ContextQuestionApi, AnswersAp
 # from services.reset_password import ForgotPassword, ResetPassword
 
 def initialize_routes(api):
-    api.add_resource(MoviesApi, '/api/movies')
-    api.add_resource(MovieApi, '/api/movie/<id>')
 
     api.add_resource(SignupApi, '/api/register')
     api.add_resource(LoginApi, '/api/login')
@@ -15,6 +12,3 @@ def initialize_routes(api):
     api.add_resource(ContextQuestionsApi, '/api/cq')
     api.add_resource(ContextQuestionApi, '/api/cq/<id>')
     api.add_resource(AnswersApi, '/api/getCorrectAnswers')
-
-    # api.add_resource(ForgotPassword, '/api/auth/forgot')
-    # api.add_resource(ResetPassword, '/api/auth/reset')
